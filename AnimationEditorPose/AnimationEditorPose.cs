92c2c98188af2cfae1bcae31fc835527096fc819
Fredrik Holmström <fredrik.johan.holmstrom@gmail.com>
2017-08-28 09:07:06 +0200

    added AnimationEditorPose

---
+++ AnimationEditorPose/AnimationEditorPose.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AnimationEditorPose : MonoBehaviour {
+  public Animator Animator;
+  public AnimationClip Clip;
+  public Single Time;
+
+  void Update() {
+    if (Application.isPlaying) {
+      Destroy(this);
+      return;
+    }
+
+#if UNITY_EDITOR
+    if (Clip) {
+      // try to grab animator
+      if (!Animator) {
+        Animator = gameObject.GetComponentInParent<Animator>();
+
+        if (!Animator) {
+          Animator = gameObject.GetComponentInChildren<Animator>();
+
+          if (!Animator) {
+            return;
+          }
+        }
+      }
+
+
+      Time = Mathf.Clamp(Time, 0, Clip.length);
+      Clip.SampleAnimation(Animator.gameObject, Time);
+    }
+#endif
+  }
+}
---
+++ AnimationEditorPose/Editor/AnimationEditorPoseEditor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+[CustomEditor(typeof(AnimationEditorPose))]
+public class AnimationEditorPoseEditor : Editor {
+  public override void OnInspectorGUI() {
+    base.OnInspectorGUI();
+
+    // grab poser component
+    var poser = target as AnimationEditorPose;
+    if (!poser) {
+      return;
+    }
+
+    // try to grab animator
+    var animator = poser.gameObject.GetComponentInParent<Animator>();
+
+    if (!animator) {
+      animator = poser.gameObject.GetComponentInChildren<Animator>();
+
+      if (!animator) {
+        return;
+      }
+    }
+
+    // grab animator controller
+    var ac = (AnimatorController)animator.runtimeAnimatorController;
+
+
+    foreach (var layer in ac.layers) {
+      foreach (var state in layer.stateMachine.states) {
+        var clip = state.state.motion as AnimationClip;
+        if (clip) {
+          if (GUILayout.Button(state.state.name)) {
+            poser.Clip = clip;
+          }
+        }
+      }
+    }
+  }
+}
